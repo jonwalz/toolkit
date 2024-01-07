ed23eb9586379a1fbde73af28207c0645deeacd7
jonwalz <jon.walz.music@gmail.com>
2024-01-07 15:36:10 -0800

    remove auth callback google login

--- src/components/user-auth-form/index.tsx
+++ src/components/user-auth-form/index.tsx
@@ -97,7 +97,6 @@ export function UserAuthForm({
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
         queryParams: {
           access_type: "offline",
           prompt: "consent",
