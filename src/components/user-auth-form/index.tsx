785f9f9dfe2fb1d7e4759f50282272663da019da
jonwalz <jon.walz.music@gmail.com>
2024-01-07 12:46:48 -0800

    fix: add redirect

--- src/components/user-auth-form/index.tsx
+++ src/components/user-auth-form/index.tsx
@@ -97,6 +97,7 @@ export function UserAuthForm({
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
+        redirectTo: `${location.origin}/auth/callback`,
         queryParams: {
           access_type: "offline",
           prompt: "consent",
