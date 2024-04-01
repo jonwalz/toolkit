3c7ccc7b4068d50ae8f82fb95a0d41f88bb8394d
jonwalz <jon.walz.music@gmail.com>
2024-04-01 16:33:37 +0200

    remove unused router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,9 @@
 import { userRouter } from "@/server/api/routers/user";
 import { createTRPCRouter } from "@/server/api/trpc";
-import { progressRouter } from "./routers/progress";
 import { accomplishmentsRouter } from "./routers/accomplishments";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
-  progress: progressRouter,
   accomplishments: accomplishmentsRouter,
 });
 
