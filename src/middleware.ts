ea49879971685323d29d692367d69a9bac6b835b
jonwalz <jon.walz.music@gmail.com>
2024-05-17 12:44:00 +0200

    add confirm route to middleware exceptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -101,6 +101,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!_next/static|_next/image|favicon.ico|auth/callback).*)",
+    "/((?!_next/static|_next/image|favicon.ico|auth/callback|auth/confirm).*)",
   ],
 };
